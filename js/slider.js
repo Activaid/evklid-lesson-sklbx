07b952d4c6d95c98d24b525c380ab1f1841c8687
Andrey Smiyan <smiyan.as@mail.ru>
2022-12-12 15:33:13 +0300

    make lesson

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,10 @@
-const swiper=new Swiper(".hero__swiper",{speed:500,pagination:{el:".hero__pagination",clickable:!0},a11y:{paginationBulletMessage:"Перейти к слайду{{index}}"}});
+const swiper = new Swiper('.hero__swiper', {
+  speed: 500,
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+  },
+  a11y: {
+    paginationBulletMessage: 'Перейти к слайду{{index}}',
+  },
+})
